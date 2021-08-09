a69976e1c3dcc8d1f3b3b82cc845bab47482c34d
szdziedzic <sz.dziedz@gmail.com>
2021-08-09 09:01:00 +0200

    render lists

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,6 +1,16 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, Image } from 'react-native';
 import { MEALS } from '../data/dummy-data';
+import DefaultText from '../components/DefaultText';
+import Colors from '../constants/Colors';
+
+const ListItem = (props) => {
+  return (
+    <View style={styles.listItem}>
+      <DefaultText>{props.children}</DefaultText>
+    </View>
+  );
+};
 
 const MealDetailScreen = (props) => {
   const mealId = props.route.params.mealId;
@@ -8,17 +18,52 @@ const MealDetailScreen = (props) => {
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
   return (
-    <View style={StyleSheet.screen}>
-      <Text>{selectedMeal.title}</Text>
-    </View>
+    <ScrollView>
+      <Image source={{ uri: selectedMeal.imageURL }} style={styles.image} />
+      <View style={styles.details}>
+        <DefaultText style={styles.mealDetailText}>
+          {selectedMeal.duration} min
+        </DefaultText>
+        <DefaultText style={styles.mealDetailText}>
+          {selectedMeal.complexity.toUpperCase()}
+        </DefaultText>
+        <DefaultText style={styles.mealDetailText}>
+          {selectedMeal.affordabillity.toUpperCase()}
+        </DefaultText>
+      </View>
+      <Text style={styles.title}>Indigrients</Text>
+      {selectedMeal.ingridents.map((indigrient) => (
+        <ListItem key={indigrient}>{indigrient}</ListItem>
+      ))}
+      <Text style={styles.title}>Steps</Text>
+      {selectedMeal.steps.map((step) => (
+        <ListItem key={step}>{step}</ListItem>
+      ))}
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  title: {
+    fontFamily: 'open-sans-bold',
+    fontSize: 22,
+    textAlign: 'center',
+  },
+  image: {
+    width: '100%',
+    height: 200,
+  },
+  details: {
+    flexDirection: 'row',
+    padding: 15,
+    justifyContent: 'space-around',
+  },
+  listItem: {
+    marginVertical: 10,
+    marginHorizontal: 20,
+    borderColor: Colors.accentColor,
+    borderWidth: 1,
+    padding: 10,
   },
 });
 
