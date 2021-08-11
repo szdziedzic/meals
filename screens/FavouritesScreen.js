f5b1f9e68b00bf58dbf37b9278cfb8174b99b26a
szdziedzic <sz.dziedz@gmail.com>
2021-08-11 12:14:03 +0200

    add fallback text for favs

--- screens/FavouritesScreen.js
+++ screens/FavouritesScreen.js
@@ -6,8 +6,24 @@ import { useSelector } from 'react-redux';
 const FavouritesScreen = (props) => {
   const favMeals = useSelector((state) => state.meals.favoriteMeals);
 
+  if (favMeals.length === 0 || !favMeals) {
+    return (
+      <View style={styles.content}>
+        <Text>No Favourite Meals Found. Start Adding some!</Text>
+      </View>
+    );
+  }
+
   return <MealsList listData={favMeals} navigation={props.navigation} />;
 };
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 
 export default FavouritesScreen;
